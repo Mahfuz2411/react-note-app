1e58f1c661b1c48c91d55b495ad17d74c83ce358
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-11-10 03:53:52 +0600

    version 1.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer footer-center w-full h-full p-4 bg-base-300 text-base-content">
             <aside>
-                <p>v-0.1 - Copyright © 2023 - All right reserved by <Link to="https://github.com/mahfuz2411">Mahfuz Ibne Syful</Link></p>
+                <p>v-1.0 - Copyright © 2023 - All right reserved by <Link to="https://github.com/mahfuz2411">Mahfuz Ibne Syful</Link></p>
             </aside>
         </footer>
     );
