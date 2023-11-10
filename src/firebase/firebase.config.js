52995e9e6644977bd494c03ed5f5ef04535aab94
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-11-11 05:21:38 +0600

    environment variables added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8FAm6DHsBx96ZWOjbMSjOQWKrqdix4To",
-  authDomain: "react-note-app-68bd9.firebaseapp.com",
-  projectId: "react-note-app-68bd9",
-  storageBucket: "react-note-app-68bd9.appspot.com",
-  messagingSenderId: "1049939976987",
-  appId: "1:1049939976987:web:050b0d31cab59cb6d329be",
-  measurementId: "G-Y4RHT9HG2Z"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
